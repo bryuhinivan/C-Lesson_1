7380934eef2d8807f86438f952cd82400d3314d2
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-05-24 21:18:21 +0500

    Changes 24.05.2023

---
+++ My_Practical_Works/Two_numbers_max/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача. Составить алгоритм определения наибольшего числа из 2 рандомных чисел.
+
+Console.Clear();
+
+int numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+
+if (numberA > numberB)
+Console.WriteLine("Наибольшее число: " + numberA);
+if (numberB > numberA)
+Console.WriteLine("Наибольшее число: " + numberB);
+if (numberA == numberB)
+Console.WriteLine("Числа равны и имеют значение: " + numberA);
+
