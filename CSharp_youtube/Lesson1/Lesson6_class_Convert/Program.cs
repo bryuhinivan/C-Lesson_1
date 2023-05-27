31a400ba4a84046d2f085015d7776d3e51097291
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-05-27 20:21:41 +0500

    Конвертация строки, класс convert

---
+++ CSharp_youtube/Lesson1/Lesson6_class_Convert/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+/*
+ * конвертация строки
+ * класс Convert
+ */
+
+
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string str;
+
+            int a, b;
+            
+            System.Console.WriteLine("Введите число 1: ");
+
+            str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+
+            System.Console.WriteLine("Введите число 2:");
+
+            str = Console.ReadLine();
+
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+
+            System.Console.WriteLine("Сумма чисел: " + result);*/
+
+            string str = "1.9";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+            double a = Convert.ToDouble(str, numberFormatInfo);
+            System.Console.WriteLine(a);
+        }
+    }
+}
---
+++ CSharp_youtube/Lesson1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * конвертация строки
+ * класс Convert
+ */
+
+
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            int a, b;
+            
+            System.Console.WriteLine("Введите число 1: ");
+
+            str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+
+            System.Console.WriteLine("Введите число 2:");
+
+            str = Console.ReadLine();
+
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+
+            System.Console.WriteLine("Сумма чисел: " + result);
+        }
+    }
+}
