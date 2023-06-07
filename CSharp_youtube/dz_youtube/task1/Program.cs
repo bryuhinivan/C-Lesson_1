8451c353a555bda58c9a94df2054326b83226cfc
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-06-07 19:50:03 +0500

    added a solution for finding the arithmetic mean of two numbers. task1

---
+++ CSharp_youtube/dz_youtube/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, вычисляющую среднее арифметическое двух чисел.
+
+Console.Clear();
+
+string str;
+
+double a, b;
+
+Console.WriteLine("Введите число 1: ");
+str = Console.ReadLine();
+a = Convert.ToDouble(str);
+
+Console.WriteLine("Введите число 2: ");
+str = Console.ReadLine();
+b = Convert.ToDouble(str);
+double result = (a + b)/2;
+
+Console.WriteLine($"Среднее арифметическиое двух чисел: {result}");
