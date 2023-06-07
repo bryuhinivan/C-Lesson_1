d1f9db6fd395e661692eff8a5222aa67b0ea8dfb
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-06-07 19:54:11 +0500

    finding summ and multi 3 numbers. task2

---
+++ CSharp_youtube/dz_youtube/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Введите три числа и выведите на экран значение суммы
+// и произведения этих чисел.
+
+Console.Clear();
+
+string str;
+
+int a, b, c;
+
+Console.WriteLine("Введите число 1: ");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+
+Console.WriteLine("Введите число 2: ");
+str = Console.ReadLine();
+b = Convert.ToInt32(str);
+
+Console.WriteLine("Введите число 3: ");
+str = Console.ReadLine();
+c = Convert.ToInt32(str);
+
+int summ = a + b + c;
+int multi = a * b * c;
+
+Console.WriteLine($"Сумма чисел: {summ}");
+Console.WriteLine($"Произведение чисел: {multi}");
+
+
